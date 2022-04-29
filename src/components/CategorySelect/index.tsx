2c4e3703ee23e569abfb47530198328d31b5ab9d
Moisés <moisesnetored@gmail.com>
2022-04-29 10:12:46 -0400

    feat: Category created.

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { View, ScrollView } from 'react-native';
 
 import {styles} from './styles';
+import {categories} from '../../utils/categories';
+import { Category } from '../Category';
 
-export function CategorySelect(){
+type Props = {
+  categorySelected: string;
+  setCategory: (categoryId: string) => void; 
+}
+
+export function CategorySelect({categorySelected, setCategory}: Props){
   return(
     <ScrollView 
       horizontal
@@ -11,7 +18,17 @@ export function CategorySelect(){
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={{paddingRight: 40}}
     >
-      
+      {
+        categories.map(category => (
+          <Category 
+            key={category.id}
+            title={category.title}
+            icon={category.icon}
+            checked={category.id === categorySelected}
+            onPress={() => setCategory(category.id)}
+          />
+        ))
+      }
     </ScrollView>
   );
 }
